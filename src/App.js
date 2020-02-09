d31863d8cc848ff1eaaf2e4f649f3afb72d1adc1
murat <muratdemir.pau@gmail.com>
2020-02-09 21:56:03 +0300

    09022020_2156

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import { Header, Icon, List } from 'semantic-ui-react';
 
 const App = () => {
-  return <div className='App'></div>;
+  const [values, setValues] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/values').then(res => {
+      setValues(res.data);
+    });
+  }, []);
+  return (
+    <div>
+      <Header as='h2'>
+        <Icon name='users' />
+        <Header.Content>Reactivities</Header.Content>
+      </Header>
+      <List>
+        {values.map(v => {
+          return <List.Item key={v.id}>{v.name}</List.Item>;
+        })}
+      </List>
+    </div>
+  );
 };
 
 export default App;
