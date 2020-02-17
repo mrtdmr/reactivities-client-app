4fb091265f0927691f655a019b9c93eb74fc8f8f
murat <muratdemir.pau@gmail.com>
2020-02-18 00:01:27 +0300

    18022020

--- src/api/agent.js
+++ src/api/agent.js
@@ -33,23 +33,23 @@ const requests = {
   get: (url: string) =>
     axios
       .get(url)
-      .then(sleep(1000))
+      //.then(sleep(1000))
       .then(responseBody),
   post: (url: string, body: {}) =>
     axios
       .post(url, body)
-      .then(sleep(1000))
+      //.then(sleep(1000))
       .then(responseBody),
   put: (url: string, body: {}) => {
     axios
       .put(url, body)
-      .then(sleep(1000))
+      //.then(sleep(1000))
       .then(responseBody);
   },
   delete: (url: string) =>
     axios
       .delete(url)
-      .then(sleep(1000))
+      //.then(sleep(1000))
       .then(responseBody)
 };
 
